3e1873c30efbe8ae018b73de338bb3e3bb9a5342
Jonathan <jonathan3692bf@users.noreply.github.com>
2021-01-08 13:25:58 +0100

    Add input field and button for making searching interactive

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import authors from '../data/authors.csv'
 import books from '../data/books.csv'
 import magazines from '../data/magazines.csv'
@@ -14,6 +14,8 @@ const Magazines = joinAuthors(magazines, 'magazine', authors)
 const AllItems = Books.concat(Magazines)
 
 const App = () => {
+  const [authorEmail, setAuthorEmail] = useState('null-lieblich@echocat.org')
+  const [searchResults, setSearchResults] = useState(findItemsByAuthorEmail(AllItems, 'null-lieblich@echocat.org'))
   console.log('Step 1: print all items but with author enriched and type of item explicit');
   console.log(AllItems)
   console.log('Step 2: find a book/magazine by the ISBN: 4545-8558-3232')
@@ -22,8 +24,25 @@ const App = () => {
   console.log(findItemsByAuthorEmail(AllItems, 'null-lieblich@echocat.org'))
   console.log('Step 4: sort all items by title (in ascending order)')
   console.log(sortItemsByKey(AllItems, 'title', 'asc'))
-  return <>
-    <h1>Step 1: print all items but with some added data (i.e. full author info and item type)</h1>
+
+  function handleChange (evt) {
+    setAuthorEmail(evt.target.value)
+    setSearchResults(findItemsByAuthorEmail(AllItems, evt.target.value))
+  }
+
+  function handleClick () {
+    setAuthorEmail('')
+    setSearchResults(AllItems)
+  }
+  return <div style={{ padding: '2rem' }}>
+    <h2 style={{ textAlign: 'center' }}>See <code>console log</code> for steps 1, 2, 3, 4</h2> 
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
+    <h3 style={{ marginRight: '1rem' }}>Step 3: find all books/magazines by the author email address:</h3>
+    <input value={authorEmail} onChange={handleChange} style={{ width: 200, marginRight: '1rem' }}/>
+    <button onClick={handleClick}>
+      Clear results
+    </button>
+    </div>
     <table>
       <thead>
         <tr>
@@ -36,11 +55,11 @@ const App = () => {
         </tr>
       </thead>
       <tbody>
-        {AllItems.map(item => {
-          return <tr>
+        {searchResults.map(item => {
+          return <tr key={item.isbn}>
             <td>{item.type}</td>
             <td>{item.title}</td>
-            <td>{item.authors.map(({ firstname, lastname }) => <div>
+            <td>{item.authors.map(({ firstname, lastname }) => <div key={lastname}>
               {`${lastname}, ${firstname}`}
             </div>)}</td>
             <td>{item.description}</td>
@@ -50,8 +69,7 @@ const App = () => {
         })}
       </tbody>
     </table>
-    
-  </>
+  </div>
 };
 
 export default App;
