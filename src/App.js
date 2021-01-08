35b25ea017a6e920a52eeb0c908fb4cc0f4eb989
Jonathan <jonathan3692bf@users.noreply.github.com>
2021-01-08 11:43:57 +0100

    Step 1 -- import CSVs and log enriched output to console

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import authors from '../data/authors.csv'
+import books from '../data/books.csv'
+import magazines from '../data/magazines.csv'
+
+function joinAuthors(collection, authors) {
+  return collection.map(item => {
+    const itemAuthorsEmails = item.authors.split(',')
+    item.authors = itemAuthorsEmails.map(authorEmail =>
+      authors.find(({ email }) =>
+        email === authorEmail
+      )
+    )
+    return item
+  })
+}
+
 const App = () => {
-  console.log('Hello world!');
+  console.log('Step 1: print all items');
+  console.log(joinAuthors(books.concat(magazines), authors))
 };
 
 export default App;
